61da261525ead837959d81a3c553aaa4ad300173
XCAGO <137565983+discordevs@users.noreply.github.com>
2025-05-14 21:49:44 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ import { getDatabase, ref, set, push } from "https://www.gstatic.com/firebasejs/
 
 // تهيئة Firebase
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    databaseURL: import.meta.env.VITE_DATABASE_URL,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID,
-    measurementId: import.meta.env.VITE_MEASUREMENT_ID
+    apiKey: "AIzaSyDzdJHG0BhmEIR4WYBWPBuM9_rprmXcqa8",
+    authDomain: "xcago-cheat.firebaseapp.com",
+    databaseURL: "https://xcago-cheat-default-rtdb.firebaseio.com",
+    projectId: "xcago-cheat",
+    storageBucket: "xcago-cheat.firebasestorage.app",
+    messagingSenderId: "617002763147",
+    appId: "1:617002763147:web:89ac6e10467bd21120658e",
+    measurementId: "G-30DXX1MVQN"
 };
 
 const app = initializeApp(firebaseConfig);
